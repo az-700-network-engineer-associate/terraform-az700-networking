d2c5823af57a9d79e744e525216155ee8db3a0cd
Devops-Developer909 <devopsdeveloper909@gmail.com>
2026-04-25 16:27:22 +0530

    Corrected names

--- envs/dev/private-link-service/backend.tf
+++ envs/dev/private-link-service/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-pls-dev"
-    storage_account_name = "storageacttfstateplsdev"
-    container_name       = "tfstate-container-pls-dev"
-    key                  = "dev-pls.tfstate"
+    storage_account_name = "storageacttfstatedev"
+    container_name       = "tf-state-pls-dev"
+    key                  = "pls-dev.tfstate"
   }
 }
