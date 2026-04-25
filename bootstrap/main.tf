9a86dd961a21895e89a6f08365045dd87ae18cd4
Devops-Developer909 <devopsdeveloper909@gmail.com>
2026-04-25 16:58:40 +0530

    updated rg name

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -5,8 +5,8 @@ resource "azurerm_resource_group" "rg-pls-bootstrap-dev" {
 
 resource "azurerm_storage_account" "tfstatestorageact" {
   name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.rg-pls-dev.name
-  location                 = azurerm_resource_group.rg-pls-dev.location
+  resource_group_name      = azurerm_resource_group.rg-pls-bootstrap-dev.name
+  location                 = azurerm_resource_group.rg-pls-bootstrap-dev.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
