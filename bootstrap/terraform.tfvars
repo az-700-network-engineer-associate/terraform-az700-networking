7dcd545c1635bcb7dbf8186f63a983a44f9cb4a3
Devops-Developer909 <devopsdeveloper909@gmail.com>
2026-04-25 15:54:16 +0530

    Added

--- bootstrap/terraform.tfvars
+++ bootstrap/terraform.tfvars
@@ -1,4 +1,4 @@
-resource_group_name = "rg-private-link-service-dev"
+resource_group_name = "rg-pls-dev"
 location = "westus2"
 storage_account_name = "storageacttfstatedev"
 storage_container_name = "tf-state-pls-dev"
--- envs/dev/private-link-service/backend.tf
+++ envs/dev/private-link-service/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-private-link-service-dev"
+    resource_group_name  = "rg-pls-dev"
     storage_account_name = "storageacttfstateplsdev"
     container_name       = "tfstate-container-pls-dev"
     key                  = "dev-pls.tfstate"
