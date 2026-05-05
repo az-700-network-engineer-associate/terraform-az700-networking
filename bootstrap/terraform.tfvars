073da9da2128c218f3bfe84d420389028059da13
Devops-Developer909 <devopsdeveloper909@gmail.com>
2026-05-05 07:45:59 +0530

    Added dynamic bootstarp

--- bootstrap/terraform.tfvars
+++
@@ -1,4 +0,0 @@
-resource_group_name = "rg-bootstrap-dev"
-location = "westus2"
-storage_account_name = "storageacttfstatedev"
-storage_container_name = "tf-state-dev"
--- envs/dev/application-gateway/backend.tf
+++ envs/dev/application-gateway/backend.tf
@@ -1,8 +1,4 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-appgw-bootstrap-dev"
-    storage_account_name = "storageacttfstatedev"
-    container_name       = "tf-state-appgw-dev"
-    key                  = "application-gateway-dev.tfstate"
   }
 }
