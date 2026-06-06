fff0644309f43edc0d1821e8dda2bde7a14f38a6
Devops-Developer909 <devopsdeveloper909@gmail.com>
2026-06-06 11:45:23 +0530

    Application-gateway-waf

---
+++ envs/dev/application-gateway-waf/main.tf
@@ -0,0 +1,19 @@
+module "application-gateway-dev" {
+  source = "../../../modules/application-gateway"
+    admin_password         = var.admin_password
+    admin_username         = var.admin_username
+    docker_password        = var.docker_password
+    docker_username        = var.docker_username
+    product_docker_image = var.product_docker_image
+    order_docker_image = var.order_docker_image
+    location               = var.location
+    resource_group_name    = var.resource_group_name
+    vmss_size              = var.vmss_size
+    appgw_name = var.appgw_name
+    appwg_subnet_address_prefix = var.appwg_subnet_address_prefix
+    vnet_name = var.vnet_name
+    vnet_address_space = var.vnet_address_space
+    backend_pool_subnet_address_prefix = var.backend_pool_subnet_address_prefix
+    
+}
+
---
+++ modules/application-gateway-waf/variables.tf
@@ -0,0 +1,62 @@
+variable "appgw_name" {
+  type = string
+}
+
+
+variable "location" {
+  type = string
+}
+
+
+
+variable "admin_password" {
+  type = string
+  description = "The password for the admin user"
+}
+variable "admin_username" {
+  type = string
+  description = "The username for the admin user"
+}
+variable "docker_username" {
+  type = string
+  description = "The username for the Docker registry"
+}
+variable "docker_password" {
+  type = string
+  description = "The password for the Docker registry"
+}
+variable "product_docker_image" {
+  type = string
+  description = "The name of the Docker image to run in the VMSS instances"
+}
+variable "order_docker_image" {
+  type = string
+  description = "The name of the Docker image to run in the VMSS instances"
+}
+
+
+variable "vmss_size" {
+  type = string
+  description = "The size of the virtual machine scale set instances"
+}
+
+variable "resource_group_name" {
+    type = string
+
+}
+
+variable "vnet_name" {
+  type = string
+}
+
+variable "vnet_address_space" {
+  type = list(string)
+}
+variable "appwg_subnet_address_prefix" {
+  type = list(string)
+}
+
+variable "backend_pool_subnet_address_prefix" {
+  type = list(string)
+}
+
